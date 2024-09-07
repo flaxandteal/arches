ecc7d973ff301b1fbf40329d06a8b796b508d8e5
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-09-07 21:13:20 +0100

    fix: add uuid to plugin views

--- arches/app/views/plugin.py
+++ arches/app/views/plugin.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+import uuid
 from django.shortcuts import render, redirect
 from django.utils.translation import gettext as _
 from arches.app.models import models

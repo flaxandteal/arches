0dbebb23731ad1aff3d4796e6be331ddc17548f4
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-11-18 20:43:50 +0200

    fix(#2650): full reports not showing

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -263,7 +263,7 @@ class Resource(models.ResourceInstance):
         self.tiles = list(models.TileModel.objects.filter(resourceinstance=self))
         if user:
             readable_nodegroups = get_nodegroups_by_perm(user, perm, any_perm=True)
-            self.tiles = [tile for tile in self.tiles if tile.nodegroup is not None and tile.nodegroup_id in readable_nodegroups]
+            self.tiles = [tile for tile in self.tiles if tile.nodegroup is not None and str(tile.nodegroup_id) in readable_nodegroups]
 
     # # flatten out the nested tiles into a single array
     def get_flattened_tiles(self):

1199471878c8d013097af54ff86edb382ab7632d
Phil Weir <phil.weir@flaxandteal.co.uk>
2026-06-01 13:19:20 +0100

    fix: do not update default value changes from shared display widgets

--- arches/app/media/js/viewmodels/widget.js
+++ arches/app/media/js/viewmodels/widget.js
@@ -126,7 +126,7 @@ var WidgetViewModel = function(params) {
             this.value(defaultValue);
         }
 
-        if (!self.form) {
+        if (!self.form && self.state !== 'display_value') {
             if (ko.isObservable(self.value)) {
                 self.valueSubscription = self.value.subscribe(function(val){
                     if (self.defaultValue() != val) {
